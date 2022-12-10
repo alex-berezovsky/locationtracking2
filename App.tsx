592b536e8972c7c0d542fb7bce7c44691d313464
Berezovsky, Alex <aberezovsky3@gatech.edu>
2022-12-09 21:28:08 -0500

    added location tracking functionality
    added location tracking functionality

--- App.tsx
+++ App.tsx
@@ -1,28 +1,86 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { useLogging } from './src/hooks/useLogging';
+// import { StatusBar } from 'expo-status-bar';
+// import React, { useEffect } from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+// import { useLogging } from './src/hooks/useLogging';
+
+// export default function App() {
+//     const [logging] = useLogging('Application');
+
+//     useEffect(() => {
+//         logging.info('Loading application.');
+//     }, [logging]);
+
+//     return (
+//         <View style={styles.container}>
+//             <Text>Open up App.tsx to start working on your app!</Text>
+//             <StatusBar style="auto" />
+//         </View>
+//     );
+// }
+
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         backgroundColor: 'cyan',
+//         alignItems: 'center',
+//         justifyContent: 'center'
+//     }
+// });
+
+
+
+import React, { useState, useEffect } from 'react';
+import { Platform, Text, View, StyleSheet } from 'react-native';
+import * as Location from 'expo-location';
 
 export default function App() {
-    const [logging] = useLogging('Application');
 
-    useEffect(() => {
-        logging.info('Loading application.');
-    }, [logging]);
 
-    return (
-        <View style={styles.container}>
-            <Text>Open up App.tsx to start working on your app!</Text>
-            <StatusBar style="auto" />
-        </View>
-    );
+  const [location, setLocation] = React.useState<any | null>(null);
+  const [errorMsg, setErrorMsg] = React.useState<string | null>(null);
+
+  React.useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+
+      let backPerm = await Location.requestBackgroundPermissionsAsync();
+      console.log(backPerm);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>{text}</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'cyan',
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: 50,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
 });
